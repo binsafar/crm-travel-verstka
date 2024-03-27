73a6b0dd50ed10a3664e7f9ebe8bda510b4bae15
binsafar <yoqub_front@olcha.uzclear>
2024-03-27 11:35:41 +0500

    added footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,27 +2,29 @@
   <footer class="footer-container">
     <div class="footer">
       <div class="footer__first">
-        <div>
-          <img src="../assets/img/logo-transparent.svg" alt="">
-          <p>Профильная CRM для турбизнеса</p>
+        <div class="footer__logo">
+          <img class="footer__svg" src="../assets/img/logo-transparent.svg" alt="">
+          <p class="footer__title">Профильная CRM для турбизнеса</p>
         </div>
 
-        <div>
+        <div class="footer__links">
           <a class="footer__link" href="">Возможности</a>
           <a class="footer__link" href="">Тариф</a>
           <a class="footer__link" href="">Контакты</a>
         </div>
 
-        <div>
+        <div class="footer__contacts">
           <a class="footer__link" href="tel:+998977632244">+998977632244</a>
-          <a class="footer__social" href=""><img src="../assets/img/facebook.svg" alt=""></a>
-          <a class="footer__social" href=""><img src="../assets/img/telegram.svg" alt=""></a>
-          <a class="footer__social" href=""><img src="../assets/img/instagram.svg" alt=""></a>
+          <div class="footer__socials">
+            <a class="footer__social" href=""><img src="../assets/img/facebook.svg" alt=""></a>
+            <a class="footer__social" href=""><img src="../assets/img/telegram.svg" alt=""></a>
+            <a class="footer__social" href=""><img src="../assets/img/instagram.svg" alt=""></a>
+          </div>
         </div>
       </div>
 
       <div class="footer__second">
-        <p>&copy; 2024 CRM TRAVEL</p>
+        <p class="footer__copyright">&copy; 2024 CRM TRAVEL</p>
         <a class="footer__link" href="mailto:info@crmtravel.uz">info@crmtravel.uz</a>
         <a class="footer__link" href="">Договор оферты</a>
         <a class="footer__link" href="">Политика конфиденциальности</a>
